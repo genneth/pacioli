d2d6a9242d12bd38910e082e9a05b34b5cfc5dc8
Gen Zhang <genneth@gmail.com>
2026-02-06 02:11:45 +0000

    Fix syntax error in list_categories.py

--- list_categories.py
+++ list_categories.py
@@ -2,8 +2,7 @@ from transaction_manager import TransactionManager
 
 def main():
     tm = TransactionManager()
-    print("
-Master Category List:")
+    print("\nMaster Category List:")
     for cat in tm.categories:
         print(f" - {cat}")
 
